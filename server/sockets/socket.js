6e9e071360d68e4b573d414a6b6ca5b4c636fb2c
Emmanuel Castro <emmanuel.castro@accenture.com>
2020-02-03 14:31:59 -0500

    commit

---
+++ server/sockets/socket.js
@@ -0,0 +1,43 @@
+const { io } = require('../server');
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../utils/utilidades');
+
+const usuarios = new Usuarios();
+io.on('connection', (client) => {
+
+    console.log('Usuario conectado');
+
+   client.on('entrarChat', (data, callback) => {
+       if (!data.nombre || !data.sala) {
+           return callback({
+               error: true,
+               mensaje: 'El nombre/sala es necesario'
+           })
+       }
+
+       client.join(data.sala);
+       let personas = usuarios.agregarPersona( client.id, data.nombre, data.sala);
+       client.broadcast.to(persona.sala).emit('listaPersona', usuarios.getPersonaPorSala(data.sala))
+       callback(usuarios.getPersonaPorSala(data.sala));
+   });
+
+   client.on('crearMensaje', (data) => {
+    let persona = usuarios.getPersona(client.id);
+    let mensaje = crearMensaje(persona.nombre, data.mensaje);
+    client.broadcast.emit('crearMensaje', mensaje);
+   });
+
+   client.on('disconnect', () => {
+       let personaBorrada = usuarios.borrarPersona(client.id);
+       client.broadcast.emit('crearMensaje', crearMensaje('Administrador', `${personaBorrada.nombre} salió`));
+       client.broadcast.emit('listaPersona', usuarios.getPersonaPorSala(personaBorrada.sala));
+
+   });
+
+   // Mensaje privado
+   client.on('mensajePrivado', data => {
+    let persona = usuarios.getPersona(client.id);
+    client.broadcast.to(data.para).emit('mensajePrivado', crearMensaje(persona.nombre, data.mensaje));
+   });
+
+});
